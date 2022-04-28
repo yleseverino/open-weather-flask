94c8fbc7c8e55d102f8feb548d1ab9a340a8e6c7
yleseverino <ylewap0@gmail.com>
2022-04-28 15:26:57 -0300

    create a validator for the country field

--- openWeatherFlask/models.py
+++ openWeatherFlask/models.py
@@ -1,11 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 from datetime import datetime
+from alpha2_to_alpha3 import alpha2_to_alpha3
 
 
 class City(BaseModel):
     id: int
-    name:str
-    country:str
+    name: str
+    country: str
+
+    @validator('country')
+    def country_must_be_alpha_3_format(cls, v):
+        if v not in alpha2_to_alpha3.values():
+            raise ValueError('country must be alpha 3 format (ISO 3166-1)')
+        return v
 
 class CityTemp(BaseModel):
     id: int
