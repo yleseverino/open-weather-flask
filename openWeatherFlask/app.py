76f862e3c47f0c7cfe99603f52a9f9c7ebd33b10
yleseverino <ylewap0@gmail.com>
2022-04-28 15:30:34 -0300

    handling the exceptions gracefully

--- openWeatherFlask/app.py
+++ openWeatherFlask/app.py
@@ -1,10 +1,12 @@
 from os import environ
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from cachetools import TTLCache
+import requests
 
 from util import get_openWeatherData
+from exceptions import CityNotFounded, InvalidAPIKey, RateLimit
 
 OPEN_WEATHER_API_KEY = environ.get('OPEN_WEATHER_API_KEY')
 
@@ -48,19 +50,35 @@ def hello(city_name = None):
 
         list_data = []
         max_itens = min( int( request.args.get('max', DEFAULT_MAX_NUMBER ) ), cache.currsize ) # First is checked if the the GET param 'max' is being send, if not a DEFAULT_MAX_NUMBER defined by env variables is used istead, and finaly is compare that number with the lenght of the cache and get the min value between the two
-        i = 1                       
+                  
         for key in cache: 
             list_data.append(cache[key].dict())
-            if i == max_itens:
-                break
-            i += 1
+
         list_data.reverse()
         
-        return { 'cities_cached' : list_data}
+        return { 'cities_cached' : list_data[:max_itens]}
 
 
-    city_name = city_name.strip().capitalize() # Format the city string name
+    city_name = city_name.strip().title() # Format the city string name
     if city_name not in cache:
-        cache[city_name] = get_openWeatherData(api_key = OPEN_WEATHER_API_KEY, city_name = city_name)
+        try:
+            cache[city_name] = get_openWeatherData(api_key = OPEN_WEATHER_API_KEY, city_name = city_name)
+        except requests.exceptions.RequestException as err:
+            return {'code': 500, 'message' : str(err)}, 500
+        except CityNotFounded as city_err:
+            return {'code': 404, 'message' : str(city_err)}, 404
+        except InvalidAPIKey as err:
+            return {'code': 401, 'message' : str(err)}, 401
+        except RateLimit as err:
+            return {'code': 429, 'message' : str(err)}, 429
+        except ConnectionError as err:
+            return {'code': 500, 'message' : str(err)}, 500
+        
     
     return cache[city_name].json()
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return make_response(
+            {'error': "Rate limit reached (%s)" % e.description}, 429
+    )
